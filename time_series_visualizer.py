25f8d353c4885ffe69856c6b3463d82598ae758b
juangomez9619 <judgomezb@correo.udistrital.edu.co>
2021-05-05 22:25:22 -0500

    Line plot draw and test ok.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,22 +1,27 @@
+#%%
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
-
+#%%
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', index_col=0, parse_dates=True)
 
 # Clean data
-df = None
+df = df[
+    (df.value <= df.value.quantile(0.975)) &
+    (df.value >= df.value.quantile(0.025))
+]
 
 
 def draw_line_plot():
-    # Draw line plot
-
-
-
-
+    fig, ax = plt.subplots()
+    plt.rcParams["figure.figsize"] = (9,4)
+    plt.plot(df.index,df.value, color='red')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -24,8 +29,30 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.groupby(by=[df.index.year, df.index.month]).agg({'value':'mean'})
+    df_bar.index.set_names(["Years", "Months"], inplace=True)
+    df_bar.reset_index(inplace=True)
 
+    def number_to_name(month):
+        number_name{
+    1:'January',
+    2:'February',
+    3:'March',
+    4:'April',
+    5:'May',
+    6:'June',
+    7:'July',
+    8:'August',
+    9:'September',
+    10:'October',
+    11:'November',
+    12:'December'
+    }
+        return number_name[month]
+        
+    df_bar['Months']= df_bar.apply(
+    lambda row: number_to_name(row['Months']),
+    axis=1)
     # Draw bar plot
 
 
